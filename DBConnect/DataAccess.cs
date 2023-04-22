e9a6d6cd8d4c35b14cb7506124e3c643dfe4f30c
EnityBlackHawk <luanf2003@gmail.com>
2023-04-22 11:54:02 -0300

    Removing unused functions

--- DBConnect/DataAccess.cs
+++ DBConnect/DataAccess.cs
@@ -13,13 +13,6 @@ namespace DBTelegraph
 {
     public class DataAccess
     {
-        public List<T> GetAll<T>(T entity) where T : ModelBase
-        {
-            using (IDbConnection conn = new SqlConnection(ConfigClass.ConnectionString))
-            {
-                return conn.Query<T>($"SELECT * FROM {entity.tableName()}").ToList();
-            }
-        }
 
         public void GetAll(Table table, Database database)
         {
@@ -32,23 +25,6 @@ namespace DBTelegraph
                     var d = (IDictionary<string, object>)r;
                     table.AddRegister(d);
                 }
-            }
-        }
-
-        public void Insert<T>(T entity) where T : ModelBase
-        {
-            using (IDbConnection conn = new SqlConnection(ConfigClass.ConnectionString))
-            {
-                string e = entity.ToString();
-                conn.Query($"INSERT INTO {entity.tableName()}{entity.GetColumnsName()} VALUES {e}");
-            }
-        }
-        public void Insert<T>(T entity, string dataBaseName) where T : ModelBase
-        {
-            using (IDbConnection conn = new SqlConnection(ConfigClass.GetConnectionStringForDataBase(dataBaseName)))
-            {
-                string e = entity.ToString();
-                conn.Query($"INSERT INTO {entity.tableName()}{entity.GetColumnsName()} VALUES {e}");
             }
         }
 
@@ -84,58 +60,6 @@ namespace DBTelegraph
 
         }
 
-        public void CreateTable(Type type)
-        {
-            using (IDbConnection conn = new SqlConnection(ConfigClass.ConnectionString))
-            {
-                StringBuilder sb = new StringBuilder($"Create table {type.Name} (\n");
-                var props = type.GetProperties();
-                List<string> pks = new List<string>();
-                foreach (var prop in props)
-                {
-                    if (prop.GetCustomAttribute(typeof(IdAttribute)) != null)
-                    {
-                        pks.Add(prop.Name);
-                    }
-
-                    sb.AppendLine($"\t {prop.Name} {TypeTable.getSQLType(prop.PropertyType)},");
-                }
-                sb.Append("\t PRIMARY KEY(");
-                for (int i = 0; i < pks.Count; i++)
-                {
-                    sb.Append(pks[i]);
-                    if (i < pks.Count - 1)
-                        sb.Append(", ");
-                }
-                sb.Append("));");
-                conn.Query(sb.ToString());
-            }
-        }
-
-        //public void CreateTable(Table table)
-        //{
-        //    using (IDbConnection conn = new SqlConnection(ConfigClass.ConnectionString))
-        //    {
-        //        StringBuilder sb = new StringBuilder($"create table {table.Name} (\n");
-        //        List<string> pks = new List<string>();
-        //        foreach (var c in table.Columns)
-        //        {
-        //            if (c.Constraints.Contains(Constraints.PRIMARY_KEY))
-        //                pks.Add(c.Name);
-
-        //            sb.AppendLine($"\t {c.Name} {TypeTable.getSQLType(c)},");
-        //        }
-        //        sb.Append("\t PRIMARY KEY(");
-        //        for (int i = 0; i < pks.Count; i++)
-        //        {
-        //            sb.Append(pks[i]);
-        //            if (i < pks.Count - 1)
-        //                sb.Append(", ");
-        //        }
-        //        sb.Append("));");
-        //        conn.Query(sb.ToString());
-        //    }
-        //}
 
         public void CreateTable(Table table, Database dataBase)
         {
